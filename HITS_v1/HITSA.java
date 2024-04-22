3c21964b3715e206bf7e82e6731b3811927eb63f
Pavel-Alze <102744491+Pavel-Alze@users.noreply.github.com>
2024-04-22 15:26:03 +0300

    Update HITSA.java

--- HITS_v1/HITSA.java
+++ HITS_v1/HITSA.java
@@ -14,7 +14,7 @@ public class HITSA {
 
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
             String[] k_v = String.valueOf(value).split("\t");
-            String[] k = k_v[0].split(":");
+            //String[] k = k_v[0].split(":");
             if(k_v.length>1)                {
                 String[] values = k_v[1].split(",");
                 for (String s : values){
