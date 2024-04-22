b925842e0a680faee32ee679bbba0506110b7940
Pavel-Alze <102744491+Pavel-Alze@users.noreply.github.com>
2024-04-22 15:25:41 +0300

    Update HITSH.java

--- HITS_v1/HITSH.java
+++ HITS_v1/HITSH.java
@@ -14,15 +14,13 @@ public class HITSH {
 
         public void map(Object key, Text value, Context context) throws IOException, InterruptedException {
             String[] k_v = String.valueOf(value).split("\t");
-            String[] k = k_v[0].split(":");
-            if(k_v.length==1){
-                context.write(new Text(k[0]),new Text("-1"));
-            }else {
+            //String[] k = k_v[0].split(":");
+            if(k_v.length>=1){
                 String[] values = k_v[1].split(",");
                 for (String s : values){
                     context.write(new Text(s),new Text(k_v[0]));
                 }
-                context.write(new Text(k[0]),new Text("-1"));
+                //context.write(new Text(k[0]),new Text("-1"));
             }
         }
     }
@@ -33,13 +31,11 @@ public class HITSH {
             double hub=0;
             String res="";
             for (Text t : values){
-                if (!String.valueOf(t).equals("-1")){
                     try{
                         String[] val = String.valueOf(t).split(":");
                         hub += Double.valueOf(val[1]);
                         res+=String.valueOf(t)+",";
                     }catch (Exception e){}
-                }
             }
             context.write(new Text(String.valueOf(key)+":"+hub),new Text(res));
         }
