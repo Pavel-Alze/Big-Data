94aa1e32e1adc6675233e70060befe7cc69c08d3
Pavel-Alze <102744491+Pavel-Alze@users.noreply.github.com>
2024-04-25 05:38:51 -0400

    Update Main.java

--- HITS_v2/Main.java
+++ HITS_v2/Main.java
@@ -14,7 +14,6 @@ import java.io.IOException;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException, ClassNotFoundException {
-        # входной файл смотреть `input`
         Configuration conf = new Configuration();
         Job job = new Job(conf, "HITSA");
         job.setJarByClass(Main.class);
@@ -26,48 +25,58 @@ public class Main {
         job.setOutputValueClass(Text.class);
         FileInputFormat.addInputPath(job, new Path("/root/BigData/mysources/input.txt"));
         FileOutputFormat.setOutputPath(job, new Path("/root/BigData/mysources/ath_out"));
-        # запуск программы по обновлению авторитетности
-        # выходной файл смотреть `ath`
         if(job.waitForCompletion(true)) {
             job = new Job(conf, "NormA");
             job.setJarByClass(Main.class);
-            job.setMapperClass(Normalized.MyMapper.class);
+            job.setMapperClass(Normalized.CountMapper.class);
             job.setMapOutputKeyClass(Text.class);
             job.setMapOutputValueClass(Text.class);
-            job.setReducerClass(Normalized.MyReducer.class);
+            job.setReducerClass(Normalized.CountReducer.class);
             job.setOutputKeyClass(Text.class);
             job.setOutputValueClass(Text.class);
             FileInputFormat.addInputPath(job, new Path("/root/BigData/mysources/ath_out/part-r-00000"));
             FileOutputFormat.setOutputPath(job, new Path("/root/BigData/mysources/norma_out"));
-            # запуск нормализации
-            # выходной файл смотреть `norma`
             if(job.waitForCompletion(true)) {
-                job = new Job(conf, "HITSH");
+                job = new Job(conf, "JoinA");
                 job.setJarByClass(Main.class);
-                job.setMapperClass(HITSH.MyMapper.class);
-                job.setMapOutputKeyClass(Text.class);
-                job.setMapOutputValueClass(Text.class);
-                job.setReducerClass(HITSH.MyReducer.class);
+                job.setMapperClass(Normalized.JoinAMapper.class);
                 job.setOutputKeyClass(Text.class);
                 job.setOutputValueClass(Text.class);
-                FileInputFormat.addInputPath(job, new Path("/root/BigData/mysources/norma_out/part-r-00000"));
-                FileOutputFormat.setOutputPath(job, new Path("/root/BigData/mysources/hub_out"));
-                # запуск программы по обновлению посредничества
-                # выходной файл смотреть `hub`
+                FileInputFormat.addInputPath(job, new Path("/root/BigData/mysources/ath_out/part-r-00000"));
+                FileOutputFormat.setOutputPath(job, new Path("/root/BigData/mysources/joina_out"));
                 if(job.waitForCompletion(true)){
-                    job = new Job(conf, "NormH");
+                    job = new Job(conf, "HITSH");
                     job.setJarByClass(Main.class);
-                    job.setMapperClass(Normalized.MyMapper.class);
+                    job.setMapperClass(HITSH.MyMapper.class);
                     job.setMapOutputKeyClass(Text.class);
                     job.setMapOutputValueClass(Text.class);
-                    job.setReducerClass(Normalized.MyReducer.class);
+                    job.setReducerClass(HITSH.MyReducer.class);
                     job.setOutputKeyClass(Text.class);
                     job.setOutputValueClass(Text.class);
-                    FileInputFormat.addInputPath(job, new Path("/root/BigData/mysources/hub_out/part-r-00000"));
-                    FileOutputFormat.setOutputPath(job, new Path("/root/BigData/mysources/normh_out"));
-                    # запуск нормализации
-                    # выходной файл смотреть `normh`
-                    System.exit(job.waitForCompletion(true) ? 0 : 1);
+                    FileInputFormat.addInputPath(job, new Path("/root/BigData/mysources/joina_out/part-r-00000"));
+                    FileOutputFormat.setOutputPath(job, new Path("/root/BigData/mysources/hub_out"));
+                    if(job.waitForCompletion(true)){
+                        job = new Job(conf, "NormH");
+                        job.setJarByClass(Main.class);
+                        job.setMapperClass(Normalized.CountMapper.class);
+                        job.setMapOutputKeyClass(Text.class);
+                        job.setMapOutputValueClass(Text.class);
+                        job.setReducerClass(Normalized.CountReducer.class);
+                        job.setOutputKeyClass(Text.class);
+                        job.setOutputValueClass(Text.class);
+                        FileInputFormat.addInputPath(job, new Path("/root/BigData/mysources/hub_out/part-r-00000"));
+                        FileOutputFormat.setOutputPath(job, new Path("/root/BigData/mysources/normh_out"));
+                        if(job.waitForCompletion(true)){
+                            job = new Job(conf, "JoinH");
+                            job.setJarByClass(Main.class);
+                            job.setMapperClass(Normalized.JoinHMapper.class);
+                            job.setOutputKeyClass(Text.class);
+                            job.setOutputValueClass(Text.class);
+                            FileInputFormat.addInputPath(job, new Path("/root/BigData/mysources/hub_out/part-r-00000"));
+                            FileOutputFormat.setOutputPath(job, new Path("/root/BigData/mysources/joinh_out"));
+                            System.exit(job.waitForCompletion(true) ? 0 : 1);
+                        }
+                    }
                 }
             }
         }
